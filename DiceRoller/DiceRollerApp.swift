ef76b35f6df80dec055eaab01c34ef8f2db170a4
shivgirja <amitsrivastava115@yahoo.com>
2022-02-04 13:36:48 +0530

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Amit Shrivastava on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
